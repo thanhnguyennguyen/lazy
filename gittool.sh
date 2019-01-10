b88a36614382e542454eda6d824f66b0b76204d2
NguyenNguyen <nguyenbk92@gmail.com>
2019-01-10 17:03:59 +0700

    Git tool: add a command to comment on a pull request

--- gittool.sh
+++ gittool.sh
@@ -5,24 +5,10 @@ usage()
         -b  | --base [repo url eg: gittool -b git@github.com:thanhnguyennguyen/lazy.git]
         -d  | --done [your commit message]: commit and push to origin
         -r  | --review [pull request number]
-        -c  | --comment [pull request number] [comment message]
+        -cp  | --comment [pull request number] [comment message]: comment on a pull request
         -h | --help : print usage
     "
 }
-while [ "$1" != "" ]; do
-    case $1 in
-        -b  | --base )          base=$2
-                                git remote set-url origin $base;;
-        -d  | --done )          commit=$2;;
-        -r  | --review )        reviewNumber=$2;;
-        -c  | --comment )       commentNumber=$2
-                                comment=$3;;
-        -h  | --help )           usage
-                                exit
-                                ;;
-    esac
-    shift
-done
 
 base=$(git config --get remote.origin.url)
 if [ "$base" = "" ]
@@ -32,21 +18,45 @@ then
     exit
 fi
 
+while [ "$1" != "" ]; do
+    case $1 in
+        -b  | --base )          base=$2
+                                git remote set-url origin $base;;
+        -d  | --done )          commit=$2
+                                # commit code and push
+                                if [ "$commit" != "" ]
+                                then
+                                    git commit -m "$commit"
+                                    git push --set-upstream origin $(git branch | grep \* | cut -d ' ' -f2)
+                                    exit
+                                fi;;
+        -r  | --review )        reviewNumber=$2
+                                token=$(cat config.txt)
+                                #  start a review
+                                if [ "$reviewNumber" != "" ]
+                                then
+                                    # start review
+                                    response=$(curl -X POST https://api.github.com/repos/$base/pulls/$reviewNumber/reviews -u "$token")
+                                    (xdg-open https://github.com/$base/pull/$reviewNumber & )
+                                    exit
+                                fi;;
+        -cp  | --comment )      commentNumber=$2
+                                content=$3
+                                token=$(cat config.txt)
+                                #  submit a comment
+                                if [ "$reviewNumber" != "" ]
+                                then
+                                    # start review
+                                    response=$(curl -X POST https://api.github.com/repos/$base/pulls/$reviewNumber/reviews -u "$token")
+                                    reviewId=response.id
+                                    curl -X POST https://api.github.com/repos/$base/pulls/$reviewNumber/reviews/$reviewId/events/ -u "$token" --data "{\"body\":\"$content\", \"event\":\"COMMENT\"}"
+                                    exit
+                                fi;;
+        -h  | --help )           usage
+                                exit
+                                ;;
+    esac
+    shift
+done
 
-# commit code and push
-if [ "$commit" != "" ]
-then
-    git commit -m "$commit"
-    git push origin
-    exit
-fi
 
-token=$(cat config.txt)
-#  review
-if [ "$reviewNumber" != "" ]
-then
-    # start review
-    response=$(curl -X POST https://api.github.com/repos/$base/pulls/$reviewNumber/reviews -u "$token")
-    (xdg-open https://github.com/$base/pull/$reviewNumber & )
-    exit
-fi
