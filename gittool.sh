0fe82bf66d430a86147dadc321221d4f58410816
NguyenNguyen <nguyenbk92@gmail.com>
2019-01-26 15:01:20 +0700

    Git tool: create PR using Git tool

--- gittool.sh
+++ gittool.sh
@@ -2,23 +2,25 @@
 usage()
 {
     echo " gittool:
-          - gittool -h  | --help : show helps
-          - gittool -d  | --done [Your commit message] : automatically commit your code and push to remote github repo (Remember to add stages in advance)
-          - gittool -r  | --review [pull request number]: start your review.
-          - gittool -ap | --approve-pull [pull request number] [comment message]: approve a pull request with a message
-          - gittool -rp | --reject-pull [pull request number] [comment message]: reject a pull request with a message
-          - gittool -ni | --new-issue [title] [content]: create new issue
-          - gittool -c  | --comment [issue/pull request number] [content]: comment on an issue/pull request
-          - gittool -cl | --close-issue [issue/pull request number] : close an issue/pull request
-          - gittool -a  | --assign [issue/pull request number] [assignee]: assign an issue/pull request to an assignee
-          - gittool -l  | --label [issue/pull request number] [label name] : label an issue/ pull request
-          - gittool -rl | --remove-label [issue/pull request number] [label name] : remove a label from an issue/ pull request
-          - gittool -m  | --merge [pull request number]: merge a pull request
-          - gittool -h  | --help : print usage
-          - gittool -v  | --version : print version
+          - gittool -h  ( --help ) : show helps
+          - gittool -d  ( --done ) [Your commit message] : automatically commit your code and push to remote github repo (Remember to add stages in advance)
+          - gittool -r  ( --review )[pull request number]: start your review.
+          - gittool -ap ( --approve-pull  ) [pull request number] [comment message]: approve a pull request with a message
+          - gittool -rp ( --reject-pull )[pull request number] [comment message]: reject a pull request with a message
+          - gittool -ni ( --new-issue )[title] [content]: create new issue
+          - gittool -c  ( --comment )[issue/pull request number] [content]: comment on an issue/pull request
+          - gittool -cl ( --close-issue ) [issue/pull request number] : close an issue/pull request
+          - gittool -a  ( --assign )[issue/pull request number] [assignee]: assign an issue/pull request to an assignee
+          - gittool -l  ( --label ) [issue/pull request number] [label name] : label an issue/ pull request
+          - gittool -rl ( --remove-label ) [issue/pull request number] [label name] : remove a label from an issue/ pull request
+          - gittool -m  ( --merge ) [pull request number]: merge a pull request
+          - gittool -p  ( --pull ) [base branch] [title] [content (optional)]
+          - gittool -h  ( --help ) print usage
+          - gittool -v  ( --version ) print version
     "
 }
 base=""
+token=$(cat ~/git/config.txt)
 checkRepo()
 {
     base=$(git config --get remote.origin.url)
@@ -30,24 +32,31 @@ checkRepo()
     fi
     repo=$(echo $base | cut -d':' -f 2 | cut -d'.' -f 1)
 }
+createPull()
+{   
+    baseBranch=$1
+    title=$2
+    body=$content$3
+    currentBranch=$(git branch | grep \* | cut -d ' ' -f2)
+    curl -X POST https://api.github.com/repos/$repo/pulls -u "$token" -d "{\"title\":\"$title\", \"base\":\"$baseBranch\", \"head\":\"$currentBranch\", \"body\": \"$body\"}"
+}
 content="(This content is created via Gittool) \n"
 while [ "$1" != "" ]; do
     case $1 in
         -b  | --base )          base=$2
                                 git remote set-url origin $base;;
         -d  | --done )          checkRepo
-                                commit=$2
+                                commitMessage=$2
                                 # commit code and push
-                                if [ "$commit" != "" ]
+                                if [ "$commitMessage" != "" ]
                                 then
-                                    git commit -m "$commit"
+                                    git commit -m "$commitMessage"
                                     git push --set-upstream origin $(git branch | grep \* | cut -d ' ' -f2)
-                                    hub pull-request -b master -m "$commit"
+                                    createPull master $commitMessage $commitMessage
                                     exit
                                 fi;;
         -r  | --review )        checkRepo
                                 reviewNumber=$2
-                                token=$(cat ~/git/config.txt)
                                 #  start a review
                                 if [ "$reviewNumber" != "" ]
                                 then
@@ -59,7 +68,6 @@ while [ "$1" != "" ]; do
         -ap  | --approve-pull ) checkRepo
                                 pullNumber=$2
                                 content=$content$3
-                                token=$(cat ~/git/config.txt)
                                 #  submit a comment
                                 reviewId=$(curl -s -X POST https://api.github.com/repos/$repo/pulls/$pullNumber/reviews -u "$token" | jq -r '.id')
                                 echo https://api.github.com/repos/$repo/pulls/$pullNumber/reviews/$reviewId/events/ 
@@ -68,7 +76,6 @@ while [ "$1" != "" ]; do
         -rp  | --reject-pull )  checkRepo
                                 pullNumber=$2
                                 content=$content$3
-                                token=$(cat ~/git/config.txt)
                                 #  submit a comment
                                 reviewId=$(curl -s -X POST https://api.github.com/repos/$repo/pulls/$pullNumber/reviews -u "$token" | jq -r '.id')
                                 echo https://api.github.com/repos/$repo/pulls/$pullNumber/reviews/$reviewId/events/ 
@@ -77,42 +84,35 @@ while [ "$1" != "" ]; do
         -ni  | --new-issue )    checkRepo
                                 title=$2
                                 body=$content$3
-                                token=$(cat ~/git/config.txt)
                                 curl -X POST https://api.github.com/repos/$repo/issues?state=all/ -u "$token" --data "{\"title\":\"$title\", \"body\":\"$body\"}"
                                 exit;;
         -c  | --comment )       checkRepo
                                 number=$2
                                 content=$content$3
-                                token=$(cat ~/git/config.txt)
                                 #  submit a comment
                                 curl -X POST https://api.github.com/repos/$repo/issues/$number/comments?state=all/ -u "$token" -d "{\"body\":\"$content\"}"
                                 exit;;
         -a  | --assign ) number=$2
                                 checkRepo
                                 assignee=$3
-                                token=$(cat ~/git/config.txt)
                                 curl -X POST https://api.github.com/repos/$repo/issues/$number/assignees?state=all/ -u "$token" -d "{\"assignees\":\"$assignee\"}"
                                 exit;;
         -l  | --label )         checkRepo
                                 number=$2
                                 label=$3
-                                token=$(cat ~/git/config.txt)
                                 curl -X POST https://api.github.com/repos/$repo/issues/$number/labels?state=all/ -u "$token" -d "{\"labels\":[\"$label\"]}"
                                 exit;;
         -rl  | --remove-label ) checkRepo
                                 number=$2
                                 label=$3
-                                token=$(cat ~/git/config.txt)
                                 curl -X DELETE https://api.github.com/repos/$repo/issues/$number/labels?state=all/$label -u "$token"
                                 exit;;
         -cl  | --close       )  checkRepo
                                 number=$2
-                                token=$(cat ~/git/config.txt)
                                 curl -X POST https://api.github.com/repos/$repo/issues/$number?state=all -u "$token" -d "{\"state\":\"closed\"}"
                                 exit;;
         -m  | --merge       )   checkRepo
                                 number=$2
-                                token=$(cat ~/git/config.txt)
                                 curl -X PUT https://api.github.com/repos/$repo/pulls/$number/merge -u "$token"
                                 exit;;
         -s   | --sync )         checkRepo
@@ -125,6 +125,10 @@ while [ "$1" != "" ]; do
                                 exit
                                 ;;
         -v  | --version )       echo gittool v1.0.1 https://github.com/thanhnguyennguyen/lazy/blob/master/gittool.sh
+                                exit
+                                ;;
+        -p  | --pull )          checkRepo
+                                createPull $2 $3 $4
                                 exit
                                 ;;
     esac
