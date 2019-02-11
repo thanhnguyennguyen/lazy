c631ca4dd69fe070eb992e222ca5e805cc6ea176
NguyenNguyen <nguyenbk92@gmail.com>
2019-02-11 18:09:38 +0700

    Minor updates for ubuntu

--- gittool.sh
+++ gittool.sh
@@ -1,32 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 usage()
 {
     echo " gittool:
-          - gittool -a  ( --assign ) [issue/pull request number] [assignee]: assign an issue/pull request to an assignee
-          - gittool -ai ( --assigned-issues) : get list of open issues assigned to me
-          - gittool -ap ( --approve-pull ) [pull request number] [comment message]: approve a pull request with a message
-          - gittool -b  ( --base ) [git repo url] : set repo url
-          - gittool -c  ( --comment ) [issue/pull request number] [content]: comment on an issue/pull request
-          - gittool -cl ( --close-issue ) [issue/pull request number] : close an issue/pull request
-          - gittool -d  ( --done ) [Your commit message] [origin/upstream]: automatically commit your code and push to remote github repo (Remember to add stages in advance)
-                                                        - upstream: by default, create new branch in origin and pull request to upstream repository
-                                                        - origin: create PR to origin. don't touch to upstream
-                                                        if your repo is not a fork repo, let's leave it empty by default
-          - gittool -h  ( --help ) : show helps
-          - gittool -i  ( --issue ) [title] [content]: create new issue
-          - gittool -l  ( --label ) [issue/pull request number] [label name] : label an issue/ pull request
-          - gittool -m  ( --merge) [pull request number]: merge a pull request
-          - gittool -oi ( --open-issues) : get list of open issues of current repository
-          - gittool -op ( --open-pullrequests): get list of open pull request
-          - gittool -p  ( --pull ) [base branch] [title] [content (optional)]
-          - gittool -r  ( --review ) [pull request number]: start your review.
-          - gittool -rl ( --remove-label ) [issue/pull request number] [label name] : remove a label from an issue/ pull request
-          - gittool -rp ( --reject-pull ) [pull request number] [comment message]: reject a pull request with a message
-          - gittool -rr ( --review-request) [pull number] [reviewer] : request a review
-          - gittool --releases  : list all releases
-          - gittool -s  ( --sync) : sync fork repo with upstream
-          - gittool -t  ( --tag) [tag name] [release name] : tag and publish a release
-          - gittool -v  ( --version ) : print version
+      - gittool -a  ( --assign ) [issue/pull request number] [assignee]: assign an issue/pull request to an assignee
+      - gittool -ai ( --assigned-issues) : get list of open issues assigned to me
+      - gittool -ap ( --approve-pull ) [pull request number] [comment message]: approve a pull request with a message
+      - gittool -b  ( --base ) [git repo url] : set repo url
+      - gittool -c  ( --comment ) [issue/pull request number] [content]: comment on an issue/pull request
+      - gittool -cl ( --close-issue ) [issue/pull request number] : close an issue/pull request
+      - gittool -d  ( --done ) [Your commit message] [upstream/origin]: 
+         automatically commit your code and push to remote github repo (Remember to add stages in advance)
+         If your repo is fotked from another one, please add upstream by: git remote add  upstream [url]
+                                   - upstream: by default, create new branch in origin and pull request to upstream repository
+                                   - origin: create PR to origin. don't touch to upstream
+                           if your repo is not a fork repo, let's leave it empty by default
+      - gittool -h  ( --help ) : show helps
+      - gittool -i  ( --issue ) [title] [content]: create new issue
+      - gittool -l  ( --label ) [issue/pull request number] [label name] : label an issue/ pull request
+      - gittool -m  ( --merge) [pull request number]: merge a pull request
+      - gittool -mop  ( --my-open-pullrequests) : list my opening pull requests in current repo
+      - gittool -mr (--my-review): list PRs waiting for my review (in this repo)
+      - gittool -oi ( --open-issues) : get list of open issues of current repository
+      - gittool -op ( --open-pullrequests): get list of open pull request
+      - gittool -p  ( --pull ) [base branch] [title] [content (optional)]
+      - gittool -r  ( --review ) [pull request number]: start your review.
+      - gittool -rl ( --remove-label ) [issue/pull request number] [label name] : remove a label from an issue/ pull request
+      - gittool -rp ( --reject-pull ) [pull request number] [comment message]: reject a pull request with a message
+      - gittool -rr ( --review-request) [pull number] [reviewer] : request a review
+      - gittool --releases  : list all releases
+      - gittool -s  ( --sync) : sync fork repo with upstream
+      - gittool -t  ( --tag) [tag name] [release name] : tag and publish a release
+      - gittool -v  ( --version ) : print version
     "
 }
 if [ "$1" = "" ]
@@ -52,7 +56,7 @@ createPull()
 {   
     pullRepo=$(echo $base | cut -d':' -f 2 | cut -d'.' -f 1)
     upstream=$(git config --get remote.upstream.url)
-    if [[ ("$upstream" != "") && ("$4" != "origin") ]]
+    if [[ "$upstream" != "" && "$4" != "origin" ]]
     then
         pullRepo=$(echo $upstream | cut -d':' -f 2 | cut -d'.' -f 1)
     fi
@@ -239,6 +243,10 @@ while [ "$1" != "" ]; do
                                 pulls=($(cat  tempPulls.txt | tr " " "_" | tr "\n" " "))
                                 assignee=($(cat  tempAssignee.txt | tr " " "_" | tr "\n" " "))
                                 rm tempTitles.txt tempUrls.txt tempPulls.txt tempAssignee.txt
+                                if [ ${#titles[@]} = 0 ]
+                                then
+                                    echo NO OPENING ISSUE IN THIS REPO
+                                fi
                                 for ((i = 0; i < ${#titles[@]}; ++i)); do
                                     if [ "${pulls[i]}" = "null" ]
                                     then
@@ -248,6 +256,10 @@ while [ "$1" != "" ]; do
                                 exit
                                 ;;
         -ai | --assigned-issues ) response=$(curl -X GET https://api.github.com/user/issues -u "$token" | jq -r ".[] | .title, .html_url ")
+                                if [ ${#response[@]} = 0 ]
+                                then
+                                    echo NO ISSUE ASSIGNED TO YOU
+                                fi
                                 for i in "${response}"
                                 do
                                     echo "$i\n"
