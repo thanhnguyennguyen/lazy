1be16798104c7d269d67da57fe8b3fd15f0779a9
NguyenNguyen <nguyenbk92@gmail.com>
2019-03-27 09:36:21 +0700

    Update message for startMyNode script

--- startMyNodes.sh
+++ startMyNodes.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
-
+echo Starting local chain ...
 gnome-terminal --geometry=150x50 \
 --tab  --title="Bootnode"   -- bash -c " cd ~/local_tomo/bootnode && tomo --nodekey boot.key -port 30301   " 
+echo "Bootnode has been started" 
 sleep 5s
-echo Starting...
 if [ "$1" = "fresh" ]
 then
     gnome-terminal --geometry=150x50 \
     --tab  --title="Node1"   -- bash -c " cd ~/local_tomo/node1 &&  ./clean.sh && ./start.sh   "  
+    echo "Node1 has been started"
     sleep 3s
     gnome-terminal --geometry=150x50 \
     --tab  --title="Node2"   -- bash -c "cd ~/local_tomo/node2 &&  ./clean.sh && ./start.sh   "  
+    echo "Node2 has been started"
     sleep 3s
     gnome-terminal --geometry=150x50 \
     --tab  --title="Node3"   -- bash -c "cd ~/local_tomo/node3 &&  ./clean.sh && ./start.sh   "  
+    echo "Node3 has been started"
     sleep 3s
     gnome-terminal --geometry=150x50 \
     --tab  --title="Node4"   -- bash -c "cd ~/local_tomo/node4 &&  ./clean.sh && ./start.sh   "  
+    echo "Node4 has been started"
     sleep 3s
     gnome-terminal --geometry=150x50 \
     --tab  --title="Node5"   -- bash -c "cd ~/local_tomo/node5 &&  ./clean.sh && ./start.sh   "  
+    echo "Node5 has been started"
     # sleep 3s
     # gnome-terminal --geometry=150x50 \
     # --tab  --title="Node6"   -- bash -c "cd ~/local_tomo/node6 &&  ./clean.sh && ./start.sh  "  
@@ -76,18 +81,23 @@ then
 else
     gnome-terminal --geometry=150x50 \
     --tab  --title="Node1"   -- bash -c " cd ~/local_tomo/node1  && ./start.sh   "  
+    echo "Node1 has been started"
     sleep 3s
     gnome-terminal --geometry=150x50 \
     --tab  --title="Node2"   -- bash -c "cd ~/local_tomo/node2  && ./start.sh   "  
+    echo "Node2 has been started"
     sleep 3s
     gnome-terminal --geometry=150x50 \
     --tab  --title="Node3"   -- bash -c "cd ~/local_tomo/node3  && ./start.sh   "  
+    echo "Node3 has been started"
     sleep 3s
     gnome-terminal --geometry=150x50 \
     --tab  --title="Node4"   -- bash -c "cd ~/local_tomo/node4  && ./start.sh   "  
+    echo "Node4 has been started"
     sleep 3s
     gnome-terminal --geometry=150x50 \
     --tab  --title="Node5"   -- bash -c "cd ~/local_tomo/node5  && ./start.sh   "  
+    echo "Node5 has been started"
     # sleep 3s
     # gnome-terminal --geometry=150x50 \
     # --tab  --title="Node6"   -- bash -c "cd ~/local_tomo/node6  && ./start.sh  "  
