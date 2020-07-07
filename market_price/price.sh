8fa1c847b38ca8a371cef6ceb94b487d364e9e9a
NguyenNguyen <nguyenbk92@gmail.com>
2020-07-07 17:06:34 +0700

    [market_price] update watchinglist

--- market_price/price.sh
+++ market_price/price.sh
@@ -1,4 +1,13 @@
-symbol=TOMOUSDT
+symbol=ADAUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+symbol=ATOMUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+
+symbol=BATUSDT
 price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
 echo $symbol: $price
 
@@ -6,12 +15,95 @@ symbol=BTCUSDT
 price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
 echo $symbol: $price
 
+symbol=EOSUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+symbol=ETCUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+
 symbol=ETHUSDT
 price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
 echo $symbol: $price
 
+symbol=ICXUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+
+symbol=IOSTUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+symbol=IOTAUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+symbol=KNCUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+symbol=LINKUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+symbol=LSKUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+
 symbol=LTCUSDT
 price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
 echo $symbol: $price
+
+symbol=OMGUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+symbol=ONTUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+
+symbol=TOMOUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+symbol=TRXUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+symbol=VETUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+
+
+symbol=WRXUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+symbol=XLMUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+
+symbol=XRPUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+symbol=ZECUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+symbol=ZRXUSDT
+price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=$symbol | jq -r '.price' )
+echo $symbol: $price
+
+
+
+
 
 
