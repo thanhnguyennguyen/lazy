ad49768e4b448d7de611d43bf4830c79d46f861b
NguyenNguyen <nguyenbk92@gmail.com>
2020-07-09 17:52:31 +0700

    update update market_price/price.sh

--- market_price/price.sh
+++ market_price/price.sh
@@ -1,7 +1,11 @@
-watchList=(ADA ATOM BAT BTC EOS ETC ETH ICX IOST IOTA KNC LINK LSK OMG ONT QTUM TOMO TRX VET WAVES WRX XLM XRP ZEC ZRX ) 
+watchList=(ADA ATOM BAT BTC CTSI EOS ETC ETH ICX IOST IOTA KNC LINK LSK OMG ONT QTUM TOMO TRX SXP VET WAVES WRX XLM XRP ZEC ZRX ) 
 
 for s in ${watchList[@]}; do
 	price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=${s}USDT | jq -r '.price' )
+	if [ $price = 'null' ]
+	then
+		price=$(curl -s -X GET https://api.binance.com/api/v3/ticker/price?symbol=${s}BUSD | jq -r '.price' )
+	fi
 	echo $s: $price
 done
 
